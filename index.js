6c1a116f7f90bd43587206d40a8c083270a7832b
Ilya <ilya.shuvatov1@gmail.com>
2022-12-05 11:39:23 +0100

    add second way

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const createForm = document.createElement('form')
 createForm.className = 'create-user-form'
 bodyBlock.append(createForm)
 
-
 const firstLabel = document.createElement('label')
 firstLabel.innerText = 'Имя'
 createForm.prepend(firstLabel)
@@ -29,6 +28,24 @@ const button = document.createElement('button')
 button.type = 'submit'
 button.innerText = 'Подтвердить'
 createForm.append(button)
+
+
+
+// bodyBlock.innerHTML = `<form class="create-user-form">
+//     <label>
+//         Имя
+//         <input type="text" name="userName" placeholder="Введите ваше имя">
+//     </label>
+//     <label>
+//         Пароль
+//         <input type="password" name="password" placeholder="Придумайте Пароль">
+//     </label>
+//     <button type="submit">
+//         Подтвердить
+//     </button>
+// </form>`
+
+
 
 console.log(bodyBlock)
 
